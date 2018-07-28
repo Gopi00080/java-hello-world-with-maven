7c8d9ec4b01b7ed7a40a100aa04ed2b26c1cd4da
Gopi00080 <Gopisainadh123@gmail.com>
2018-07-28 17:21:10 +0530

    what to tell

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current time is: " + currentTime);
-
+		  System.out.println("The Day is  ");
+		  
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
